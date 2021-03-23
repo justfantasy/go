f453f18cc5a401b22b70e4fb3f128f5bd4b693fe
justfantasy <27506954@qq.com>
2021-03-23 09:23:20 +0800

    'leetcode3'

--- leetcode/length_of_longest_substring_3.go
+++ leetcode/length_of_longest_substring_3.go
@@ -1,11 +1,55 @@
 package leetcode
 
+//来源https://books.halfrost.com/leetcode/ChapterFour/0001~0099/0003.Longest-Substring-Without-Repeating-Characters/
+func LengthOfLongestSubstring_(s string) int {
+	length := len(s)
+	if length == 0 {
+		return 0
+	}
+
+	var bitSet [256]bool
+	result, left, right := 0, 0, 0
+	for left < length {
+		if bitSet[s[right]] {
+			bitSet[s[left]] = false
+			left++
+		} else {
+			bitSet[s[right]] = true
+			right++
+		}
+
+		if result < right-left {
+			result = right - left
+		}
+
+		if left + result >= length || right >= length {
+			break
+		}
+	}
+	return result
+}
+
+// 使用滑动窗口算法
 func LengthOfLongestSubstring(s string) int {
 	if len(s) == 0 {
 		return 0
 	}
-
-	for i, v := range s {
-
+	var tmp = make(map[int32]int)  // 创建一个字典，存储的字符和其对应的位置
+	longest, left, right := 0, 0, 0          // 最大长度，左边界，右边界
+	for _, v := range s {
+		right++
+		if inx, ok := tmp[v]; ok {
+			if inx > left {   // 忽略超出边界的元素
+				left = inx    // 出现相同的元素，则将元素放到边界外，这样整个窗口中不存在相同的字符
+			}
+		}
+		if longest < right - left {
+			longest = right - left
+		}
+		if left + longest >= len(s) {
+			break
+		}
+		tmp[v] = right
 	}
+	return longest
 }
--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	. "p2/leetcode"
+	"p2/leetcode"
 )
 
-
 func main()  {
-	l1 := &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}}
-	l2 := &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}}
-	fmt.Println(AddTwoNumbers(l1, l2))
-	//fmt.Println(l.TwoSum([]int{1, 6, 3}, 9))
+	//l1 := &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}}
+	//l2 := &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}}
+	//fmt.Println(AddTwoNumbers(l1, l2))
+	//fmt.Println(l.TwoSum([]int{1, 6, 3}, 9)) abcedefaec
+	fmt.Println(leetcode.LengthOfLongestSubstring("pwwkewp"))
 }
